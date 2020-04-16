0a3ad29b428b55351524bc1b996da3e8cf2ace7f
Dean <38113207+ChillyMiners@users.noreply.github.com>
2020-04-16 18:32:52 +0100

    made simple doubling functionality

--- NoteKeeper2/app/src/main/java/com/example/notekeeper2/MainActivity.kt
+++ NoteKeeper2/app/src/main/java/com/example/notekeeper2/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val originalValue = textDisplayedValue.text.toString().toInt();
+            val newValue = originalValue * 2;
+            textDisplayedValue.text = newValue.toString();
+            Snackbar.make(view, "Value $originalValue changed to $newValue",
+                          Snackbar.LENGTH_LONG)
+                .show()
         }
     }
 
