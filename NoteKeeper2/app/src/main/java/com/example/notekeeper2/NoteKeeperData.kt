ecffaeff7100c5596c472f4defe21aae0bfe4870
ChillyMiners <38113207+ChillyMiners@users.noreply.github.com>
2020-05-08 01:17:16 +0100

    added recycler view

--- NoteKeeper2/app/src/main/java/com/example/notekeeper2/NoteKeeperData.kt
+++ NoteKeeper2/app/src/main/java/com/example/notekeeper2/NoteKeeperData.kt
@@ -6,6 +6,4 @@ data class CourseInfo (val courseId: String, val title:String) {
     }
 }
 
-data class NoteInfo (var course: CourseInfo? = null,
-                     var title: String? = null,
-                     var text: String? = null)
+data class NoteInfo (var course: CourseInfo? = null, var title: String? = null, var text: String? = null)
--- NoteKeeper2/app/src/main/java/com/example/notekeeper2/NoteListActivity.kt
+++ NoteKeeper2/app/src/main/java/com/example/notekeeper2/NoteListActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import kotlinx.android.synthetic.main.activity_note_list.*
 import kotlinx.android.synthetic.main.content_note_list.*
@@ -14,24 +15,17 @@ class NoteListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
         setSupportActionBar(toolbar)
-            //floating action button listener
+
         fab.setOnClickListener { view ->
             val activityIntent = Intent(this, MainActivity::class.java)
             startActivity(activityIntent)
         }
-        //populating the list view
-        listNotes.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
-            DataManager.notes)
-        //listener for list view items being selected
-        listNotes.setOnItemClickListener{parent, view, position, id ->
-            val activityIntent = Intent(this, MainActivity::class.java)
-            activityIntent.putExtra(NOTE_POSITION, position)
-            startActivity(activityIntent)
-        }
+
+        listItems.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onResume() {
         super.onResume()
-        (listNotes.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
+
     }
 }
